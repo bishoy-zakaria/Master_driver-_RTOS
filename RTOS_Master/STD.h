b541d855e25d989716b21fd9b27310e5c6e8dc25
Bishoy Zakaria <bishoyzakaria1999@gmail.com>
2020-08-24 19:46:59 +0200

    make master driver with free RTOS

---
+++ RTOS_Master/STD.h
@@ -0,0 +1,22 @@
+/*
+ * STD.h
+ *
+ *  Created on: ??þ/??þ/????
+ *      Author: Ali Shawky
+ */
+
+#ifndef STD_H_
+#define STD_H_
+
+typedef unsigned char uint8;
+typedef signed   char int8;
+
+typedef unsigned short int uint16;
+typedef signed   short int int16;
+
+typedef unsigned long int uint32;
+typedef signed   long int int32;
+
+typedef  float float32;
+
+#endif /* STD_H_ */
